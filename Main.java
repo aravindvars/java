8e1881659a12449c7c27a66ba21358e8961d8fbc
aravind <aravindramsharma@gmail.com>
2024-07-28 20:10:22 +0530

    Created separate files for cal & main

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+        public static void main (String [] args) {
+            System.out.println("java is boring, i Have to do it anyway");
+
+            Calculator calc = new Calculator();
+
+            calc.add(2,4);
+
+
+        }
+
+}
