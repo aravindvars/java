95da716ded83b0ee689eb6f8d3f9e6b0e7efce3c
Aravind Ram sharma <aravind@Aravinds-Mac-mini.local>
2024-07-28 20:00:14 +0530

    Added basic logic

--- firstOne.java
+++ firstOne.java
@@ -1,3 +1,14 @@
+
+public static void add (int a, int b){
+    int sum = 0;
+    sum = a+b;
+    System.out.println(sum);
+}
+
+
 public static void main (String [] args) {
-System.out.println("java");
+System.out.println("java is boring, i Have to do it anyway");
+
+add(2,3);
+
 }
