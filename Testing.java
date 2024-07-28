b71f3a8f909bd1acfda10727109b8928c40e7e1d
aravind <aravindramsharma@gmail.com>
2024-07-28 20:22:51 +0530

    Changed the name of the file

--- Testing.java
+++ Testing.java
@@ -1,7 +1,7 @@
 import java.util.logging.Logger;
 
-public class Main {
-    private static Logger log = Logger.getLogger(Main.class.getName());
+public class Testing {
+    private static Logger log = Logger.getLogger(Testing.class.getName());
         public static void main (String [] args) {
             System.out.println("java is boring, i Have to do it anyway");
             log.info("Calling the calc program");
