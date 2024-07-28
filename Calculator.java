33699f060390259b302b035aeb584bc86ebe5e53
aravind <aravindramsharma@gmail.com>
2024-07-28 20:12:34 +0530

    Minor Changes

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,5 @@
-
-
 public class Calculator {
     public void add(int a, int b) {
-
         int sum = a + b;
         System.out.println(sum);
     }
--- Main.java
+++ Main.java
@@ -1,13 +1,7 @@
 public class Main {
-
         public static void main (String [] args) {
             System.out.println("java is boring, i Have to do it anyway");
-
             Calculator calc = new Calculator();
-
             calc.add(2,4);
-
-
         }
-
 }
