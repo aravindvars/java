c34bc53711a48a97d32e87bc88723d3b005c39d5
aravind <aravindramsharma@gmail.com>
2024-07-28 20:21:32 +0530

    Added code to get Logger instance

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+import java.util.logging.Logger;
+
 public class Main {
+    private static Logger log = Logger.getLogger(Main.class.getName());
         public static void main (String [] args) {
             System.out.println("java is boring, i Have to do it anyway");
+            log.info("Calling the calc program");
             Calculator calc = new Calculator();
             calc.add(2,4);
         }
